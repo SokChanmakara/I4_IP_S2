722886f30ad94b3c9aedc1275d84aa3b725a2456
SokChanmakara <sokchanmakara04@gmail.com>
2025-03-25 20:09:43 +0700

    Finished Task 5

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Observers\ModelActivityObserver;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Order::observe(ModelActivityObserver::class);
     }
 }
